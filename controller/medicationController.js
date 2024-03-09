6d206faf70cc061ca3a4221736e4f034cae3b025
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-03-09 19:02:38 +0530

    Apply update a medication Function

--- controller/medicationController.js
+++ controller/medicationController.js
@@ -19,30 +19,53 @@ const createMedication = asyncHandler(async (req, res) => {
 });
 
 const getAllMedications = asyncHandler(async (req, res) => {
-    // console.log("message: all medications");
-    const medicines = await Medication.findAll();
-    res.status(200).json(medicines);
-  });
+  // console.log("message: all medications");
+  const medicines = await Medication.findAll();
+  res.status(200).json(medicines);
+});
 
-  const getAMedication = asyncHandler(async (req, res) => {
-    // console.log("get a medication");
-    const medication = await Medication.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(medication);
+const getAMedication = asyncHandler(async (req, res) => {
+  // console.log("get a medication");
+  const medication = await Medication.findOne({
+    where: {
+      id: req.params.id,
+    },
   });
+  res.status(200).json(medication);
+});
 
-  const deleteAMedication = asyncHandler(async (req, res) => {
-    //   console.log("Delete a medication");
-    const medication = await Medication.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
-    await medication.destroy();
-    res.status(200).json({ message: "medication deleted" });
+const deleteAMedication = asyncHandler(async (req, res) => {
+  //   console.log("Delete a medication");
+  const medication = await Medication.findOne({
+    where: {
+      id: req.params.id,
+    },
   });
+  await medication.destroy();
+  res.status(200).json({ message: "medication deleted" });
+});
 
-export {createMedication, getAllMedications, getAMedication, deleteAMedication};
+const updateAMedication = asyncHandler(async (req, res) => {
+//   console.log("Update a medication");
+  const medication = await Medication.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  const { name, description, quantity } = req.body;
+  await medication.set({
+    name: name,
+    description: description,
+    quantity: quantity,
+  });
+  await medication.save();
+  res.status(200).json(medication);
+});
+
+export {
+  createMedication,
+  getAllMedications,
+  getAMedication,
+  deleteAMedication,
+  updateAMedication,
+};
--- routes/medicationRoute.js
+++ routes/medicationRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { authMiddleware, isAdmin } from "../middlewares/authMiddelware.js";
-import { createMedication, deleteAMedication, getAMedication, getAllMedications } from "../controller/medicationController.js";
+import { createMedication, deleteAMedication, getAMedication, getAllMedications, updateAMedication } from "../controller/medicationController.js";
 const router = express.Router();
 
 router.get("/get-all-medications", getAllMedications);
 router.get("/get-a-medication/:id", getAMedication);
 router.post("/create-medication", createMedication);
+router.put("/update-medication/:id", updateAMedication);
 router.delete("/delete-medication/:id", deleteAMedication);
 
 export default router;

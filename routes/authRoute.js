025bbdfd9695eaef37e5da485455d813b93b7c21
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-03-10 11:05:47 +0530

    Update Routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,11 +3,11 @@ import { createUser, deleteAUser, getAUser, getAllUsers, loginUserCtrl, updateAU
 import { authMiddleware, isOwner } from "../middlewares/authMiddelware.js";
 const router = express.Router();
 
-router.get("/getAllUsers", getAllUsers);
+router.get("/getAllUsers", authMiddleware, isOwner, getAllUsers);
 router.get("/getAUser/:id", authMiddleware, isOwner, getAUser);
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
-router.put("/updateAUser/:id", updateAUser);
-router.delete("/deleteAUser/:id", deleteAUser);
+router.put("/updateAUser/:id", authMiddleware, isOwner, updateAUser);
+router.delete("/deleteAUser/:id", authMiddleware, isOwner, deleteAUser);
 
 export default router;
